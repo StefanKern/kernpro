3bef7928d1119e21e8bb3e5bdbcb3cebb2825bb2
Stefan Kern <stefankern@outlook.com>
2018-11-25 13:19:51 +0100

    Added Animation timeline images

--- src/app/components/startpage/timeline/timeline.component.ts
+++ src/app/components/startpage/timeline/timeline.component.ts
@@ -11,31 +11,5 @@ export class TimelineComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    let options = {
-      root: document.querySelector('#SidenavContent'),
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-    let entries = [0, 0.25, 0.5, 0.75, 1.0];
-    let callback = function(entries, observer) { 
-      entries.forEach(entry => {
-        debugger;
-        // Each entry describes an intersection change for one observed
-        // target element:
-        //   entry.boundingClientRect
-        //   entry.intersectionRatio
-        //   entry.intersectionRect
-        //   entry.isIntersecting
-        //   entry.rootBounds
-        //   entry.target
-        //   entry.time
-      });
-    };
-    
-    var observer = new IntersectionObserver(callback, options);
-
-    let test = document.querySelector('#test');
-    observer.observe(test);
   }
-
 }
--- src/app/directives/intersection.directive.ts
+++ src/app/directives/intersection.directive.ts
@@ -1,10 +1,39 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[coreIntersection]'
 })
 export class IntersectionDirective {
 
-  constructor() { }
+  @Input() coreIntersection: string;
 
+  constructor(el: ElementRef, private renderer: Renderer2) {    
+    renderer.addClass(el.nativeElement, "animated");
+    this.renderer.setStyle(
+      el.nativeElement,
+      'visibility',
+      'hidden'
+    );
+
+    let options = {
+      root: document.querySelector('#SidenavContent'),
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    let entries = [0.01];
+    let callback = (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {           
+          this.renderer.removeStyle(
+            el.nativeElement,
+            'visibility'
+          );
+          renderer.addClass(el.nativeElement, this.coreIntersection);
+          observer.unobserve(el.nativeElement);        }
+      });
+    };
+
+    var observer = new IntersectionObserver(callback, options);
+    observer.observe(el.nativeElement);
+  }
 }
