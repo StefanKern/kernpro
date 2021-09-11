b65b5c05f5d931546c1dbfde5502240283a1c300
stefankern@outlook.com <3v00z%wbXkjcpE#90JMs>
2021-09-11 02:50:19 +0200

    Add Photos of myslef on startpage

---
+++ src/app/components/startpage/photos-of-myself/photos-of-myself.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'photos-of-myself',
+  templateUrl: './photos-of-myself.component.html',
+  styleUrls: ['./photos-of-myself.component.scss']
+})
+export class PhotosOfMyselfComponent implements OnInit {
+  imageData = [
+    {
+      srcUrl: '/assets/photos-of-myself/outdoor.jpg',
+      previewUrl: '/assets/photos-of-myself/outdoor.jpg'
+    },
+    {
+      srcUrl: '/assets/photos-of-myself/at-we-are-developers.jpg',
+      previewUrl: '/assets/photos-of-myself/at-we-are-developers.jpg'
+    },
+    {
+      srcUrl: '/assets/photos-of-myself/hiking.jpg',
+      previewUrl: '/assets/photos-of-myself/hiking.jpg'
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
