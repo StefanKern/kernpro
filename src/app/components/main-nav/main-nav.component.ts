d9d54e7fe6e2dab2a4df375aceed6b53553515b4
stefankern@outlook.com <3v00z%wbXkjcpE#90JMs>
2021-10-25 02:15:09 +0200

    Languagepicker at the bottom of the side menu

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'core-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.scss'],
+  styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent {
   selectedLng = 'de'
