d4f6ca90e786925839d103d66e73947ab5321f44
stefankern@outlook.com <3v00z%wbXkjcpE#90JMs>
2021-11-14 16:41:41 +0100

    Translations and small improvments - add age - reworked SO counter -etc

--- src/app/components/startpage/aboutme/aboutme.component.ts
+++ src/app/components/startpage/aboutme/aboutme.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {DateTime} from 'luxon';
 
 @Component({
   selector: 'core-aboutme',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutme.component.scss']
 })
 export class AboutmeComponent implements OnInit {
+  age = Math.floor(DateTime.fromFormat('03.10.1986', 'dd.MM.yyyy').diffNow().as('years') * -1);
+  scoreOnceVisible = false;
+  @ViewChild('score', {read: ElementRef, static: true}) score: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      // root: /* needs to be the element, where the scrollbar is on. Because it is on the <html> element we dont need to set it */,
+      rootMargin: '64px',
+      threshold: 1.0
+    };
+    const callback = (entries: IntersectionObserverEntry[], observer) => {
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+          this.scoreOnceVisible = true;
+          observer.unobserve(this.score.nativeElement);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.score.nativeElement);
   }
 
 }
--- src/app/components/startpage/score/score.component.ts
+++ src/app/components/startpage/score/score.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'core-score',
@@ -9,12 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class ScoreComponent implements OnInit {
   SOReputation: number = 0;
   soTrackerComplete = false;
+  profileLink = ''
+
+  @HostListener('click') onClick() {
+    window.location.href = this.profileLink;
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get<any>('https://api.stackexchange.com/2.3/users/639035?order=desc&sort=reputation&site=stackoverflow')
-    .subscribe((result) => this.SOReputation = result.items[0]?.reputation);
+    .subscribe((result) => {
+      this.SOReputation = result.items[0]?.reputation
+      this.profileLink = result.items[0]?.link
+    });
   }
 
 }
