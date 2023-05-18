eae18c0d0fcbdbd674a6d7e6f11208e3450686d1
stefankern@outlook.com <3v00z%wbXkjcpE#90JMs>
2023-05-18 23:21:11 +0200

    ng generate @angular/material:mdc-migration

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
-import {MatLegacyCardModule as MatCardModule} from '@angular/material/legacy-card';
-import {MatLegacyListModule as MatListModule} from '@angular/material/legacy-list';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatLegacySlideToggleModule as MatSlideToggleModule} from '@angular/material/legacy-slide-toggle';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import {
   MatTreeModule,
