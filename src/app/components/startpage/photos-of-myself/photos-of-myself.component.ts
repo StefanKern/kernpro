3161aba46f21f432fb27fa31124773ec6c3a2d4b
Stefan Kern <stefankern@outlook.com>
2025-03-22 00:47:30 +0100

    Make image gallery working again

--- src/app/components/startpage/photos-of-myself/photos-of-myself.component.ts
+++ src/app/components/startpage/photos-of-myself/photos-of-myself.component.ts
@@ -1,36 +1,76 @@
-import { Component, inject } from '@angular/core';
-import { GalleryModule, Image, LineLayout, ModalGalleryRef, ModalGalleryService, PlainGalleryConfig, PlainGalleryStrategy, PlainLibConfig } from '@ks89/angular-modal-gallery';
+import { NgIf } from '@angular/common';
+import { Component, HostListener, inject, OnInit, signal } from '@angular/core';
+import { GalleryModule, GridLayout, Image, ModalGalleryRef, ModalGalleryService, PlainGalleryConfig, PlainGalleryStrategy, PlainLibConfig } from '@ks89/angular-modal-gallery';
 
 @Component({
     selector: 'core-photos-of-myself',
     templateUrl: './photos-of-myself.component.html',
     styleUrls: ['./photos-of-myself.component.scss'],
     imports: [
-      GalleryModule
+      GalleryModule,
+      NgIf
     ]
 })
-export class PhotosOfMyselfComponent {
+export class PhotosOfMyselfComponent implements OnInit {
   modalGalleryService = inject(ModalGalleryService);
 
   imageData = [
     new Image(0, {img: '/assets/photos-of-myself/outdoor.jpg'}),
     new Image(1, {img: '/assets/photos-of-myself/at-we-are-developers.jpg'}),
-    new Image(2,{img: '/assets/photos-of-myself/hiking.jpg'}),
+    new Image(2, {img: '/assets/photos-of-myself/hiking.jpg'}),
     new Image(3, {img: '/assets/photos-of-myself/lunch.jpg'})
-  ]
-  
-  plainGalleryRow: PlainGalleryConfig = { 
-    strategy: PlainGalleryStrategy.ROW,
-    layout: new LineLayout({ width: '200px', height: '200px' }, { length: 4, wrap: true }, 'space-evenly')
-  };
+  ];
 
-  libConfigPlainGalleryRow: PlainLibConfig = {
-    plainGalleryConfig: this.plainGalleryRow
-  };
-  
+  plainGalleryRow: PlainGalleryConfig;
+  libConfigPlainGallery = signal<PlainLibConfig | null>(null);
+  galleryVisible = signal(true); // Flag to control visibility of the gallery
+
+  ngOnInit(): void {
+    this.updateLayout(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    const width = (event.target as Window).innerWidth;
+    this.updateLayout(width);
+  }
+
+  private updateLayout(width: number): void {
+    let layoutWidth = '200px';
+    let layoutHeight = '200px';
+    let length = 4; // Default: all images in one row
+
+    if (width < 768) { // Small screens
+      layoutWidth = '150px';
+      layoutHeight = '150px';
+      length = 2; // Two images per row
+    }
+
+    // Only update the configuration if the length has changed
+    if (!this.plainGalleryRow || (this.plainGalleryRow.layout as GridLayout).breakConfig.length !== length) {
+      debugger;
+      this.plainGalleryRow = {
+        strategy: PlainGalleryStrategy.GRID,
+        layout: new GridLayout(
+          { width: layoutWidth, height: layoutHeight },
+          { length, wrap: true }
+        )
+      };
+
+      this.libConfigPlainGallery.set({
+        plainGalleryConfig: this.plainGalleryRow
+      });
+
+      // Force recreation of the gallery component
+      this.galleryVisible.set(false);
+      setTimeout(() => {
+        this.galleryVisible.set(true);
+      });
+    }
+  }
 
   onShow(id: number, index: number, images: Image[] = this.imageData): void {
-    const dialogRef: ModalGalleryRef = this.modalGalleryService.open({
+    this.modalGalleryService.open({
       id,
       images,
       currentImage: images[index]
