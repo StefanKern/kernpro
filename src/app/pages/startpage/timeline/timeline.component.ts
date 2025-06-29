3cf6e7a14a537120b1bba6431ee1dd8637e13603
Stefan Kern <stefankern@outlook.com>
2025-06-29 21:31:33 +0200

    Add TimelineintersectionDirective to TimelineComponent imports

--- src/app/pages/startpage/timeline/timeline.component.ts
+++ src/app/pages/startpage/timeline/timeline.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { TimelineintersectionDirective } from './timelineintersection.directive';
 
 @Component({
-    selector: 'core-timeline',
-    templateUrl: './timeline.component.html',
-    styleUrls: ['./timeline.component.scss'],
-    imports: [
-        MatIcon
-    ],
-    standalone: true
+  selector: 'core-timeline',
+  templateUrl: './timeline.component.html',
+  styleUrls: ['./timeline.component.scss'],
+  imports: [
+    MatIcon,
+    TimelineintersectionDirective
+  ],
+  standalone: true
 })
 export class TimelineComponent {
 }
