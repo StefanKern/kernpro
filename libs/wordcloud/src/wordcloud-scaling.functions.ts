cd166d182fcc6fa83632c3b181f9d0f4e761c2f8
Stefan Kern <stefankern@outlook.com>
2025-08-11 15:28:35 +0200

    remove empty file

--- libs/wordcloud/src/wordcloud-scaling.functions.ts
+++
@@ -1,4 +0,0 @@
-// Deprecated: adaptive scaling logic removed. File retained only to avoid import resolution errors if external code referenced it.
-// All previous functions have been removed intentionally.
-
-export {};
